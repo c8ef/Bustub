955810afd8ee3ad4d8c3a6bdabf68ce2297bf740
addr2line <addr2line@gmail.com>
2022-11-03 23:47:56 +0800

    finish insert

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -18,10 +18,42 @@ namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)), inserted_(false) {}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+void InsertExecutor::Init() { child_executor_->Init(); }
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+  if (inserted_) {
+    return false;
+  }
+  inserted_ = true;
+
+  Tuple child_tuple{};
+  int insert_num = 0;
+
+  std::string table_name = exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->name_;
+  std::vector<IndexInfo *> index_vector;
+  for (const auto &index : exec_ctx_->GetCatalog()->GetTableIndexes(table_name)) {
+    if (index->name_ == table_name) {
+      index_vector.push_back(index);
+    }
+  }
+
+  while (child_executor_->Next(&child_tuple, rid)) {
+    insert_num += 1;
+    exec_ctx_->GetCatalog()
+        ->GetTable(plan_->TableOid())
+        ->table_->InsertTuple(child_tuple, rid, exec_ctx_->GetTransaction());
+
+    for (auto index : index_vector) {
+      index->index_->InsertEntry(child_tuple, *rid, exec_ctx_->GetTransaction());
+    }
+  }
+
+  std::vector<Value> values{};
+  values.push_back(Value(TypeId::INTEGER, insert_num));
+  *tuple = Tuple{values, &GetOutputSchema()};
+  return true;
+}
 
 }  // namespace bustub

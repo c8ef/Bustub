3aa1626f2b1c5cc05209d23c375d9c190c4d2396
addr2line <addr2line@gmail.com>
2022-11-04 00:00:46 +0800

    finish delete operator

--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -18,10 +18,40 @@ namespace bustub {
 
 DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)), deleted_(false) {}
 
-void DeleteExecutor::Init() { throw NotImplementedException("DeleteExecutor is not implemented"); }
+void DeleteExecutor::Init() { child_executor_->Init(); }
 
-auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+  if (deleted_) {
+    return false;
+  }
+  deleted_ = true;
+
+  Tuple child_tuple{};
+  int delete_num = 0;
+
+  std::string table_name = exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->name_;
+  std::vector<IndexInfo *> index_vector;
+  for (const auto &index : exec_ctx_->GetCatalog()->GetTableIndexes(table_name)) {
+    if (index->name_ == table_name) {
+      index_vector.push_back(index);
+    }
+  }
+
+  while (child_executor_->Next(&child_tuple, rid)) {
+    delete_num += 1;
+    exec_ctx_->GetCatalog()->GetTable(plan_->TableOid())->table_->MarkDelete(*rid, exec_ctx_->GetTransaction());
+
+    for (auto index : index_vector) {
+      index->index_->DeleteEntry(child_tuple, *rid, exec_ctx_->GetTransaction());
+    }
+  }
+
+  std::vector<Value> values{};
+  values.push_back(Value(TypeId::INTEGER, delete_num));
+  *tuple = Tuple{values, &GetOutputSchema()};
+  return true;
+}
 
 }  // namespace bustub
